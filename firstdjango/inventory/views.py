8a0d6f592c4453a4d631dff431380bcb6fed1da2
Xinyang <xinyzhang9@gmail.com>
2016-05-25 15:58:31 -0700

    templates

--- firstdjango/inventory/views.py
+++ firstdjango/inventory/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from inventory.models import Item
+
 # Create your views here.
 def index(request):
-	return HttpResponse('<p>In index view</p>')
+	items = Item.objects.exclude(amount=0)
+	return render(request,'inventory/index.html',{
+		'items' : items,
+		})
 
 def item_detail(request,id):
-	return HttpResponse('<p>In item_detail view with id {0}</p>'.format(id))
+	try:
+		item = Item.objects.get(id = id)
+	except Item.DoesNotExist:
+		raise Http404('This item does not exist')
+	return render(request,'inventory/item_detail.html',{
+		'item' : item,
+		})
+
 
