545d8fd105f0d53a6c6a08014c39ff5faba26d2e
Xinyang <xinyzhang9@gmail.com>
2016-05-25 12:11:20 -0700

    admin

--- firstdjango/firstdjango/urls.py
+++ firstdjango/firstdjango/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from inventory import views
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',views.index, name = 'index'),
+    url(r'^item/(?P<id>\d+)/',views.item_detail, name = "item_detail"),
 ]
