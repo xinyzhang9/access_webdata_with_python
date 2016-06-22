fe91b3cf8e54231e41c165755b4564c40be24002
Xinyang <xinyzhang9@gmail.com>
2016-06-22 14:40:10 -0700

    finish css

--- mytutorial/polls/urls.py
+++ mytutorial/polls/urls.py
@@ -5,9 +5,9 @@ app_name = 'polls'
 urlpatterns = [
 	url(r'^$',views.IndexView.as_view(), name = 'index'),
 	# ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
