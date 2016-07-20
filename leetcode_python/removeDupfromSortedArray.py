977b7c60b575c7e70d2ef8b05aa4657b3db7bfe4
Xinyang <xinyzhang9@gmail.com>
2016-07-20 14:51:25 -0700

    remove dup from sorted array

---
+++ leetcode_python/removeDupfromSortedArray.py
@@ -0,0 +1,28 @@
+# Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+# Do not allocate extra space for another array, you must do this in place with constant memory.
+
+# For example,
+# Given input array nums = [1,1,2],
+
+# Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 1:
+            return 0
+        dic = dict()
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[nums[i]] = 1
+                nums[idx],nums[i] = nums[i],nums[idx]
+                idx += 1
+                
+        return idx
+        
+            
