541f2570f3e1ca2b13255175d43c58f31e0ec6a5
Xinyang <xinyzhang9@gmail.com>
2016-07-22 17:22:55 -0700

    triangle bottom up

---
+++ leetcode_python/triangle.py
@@ -0,0 +1,24 @@
+# Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+# For example, given the following triangle
+# [
+#      [2],
+#     [3,4],
+#    [6,5,7],
+#   [4,1,8,3]
+# ]
+# The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle:
+            return
+        res = triangle[-1]
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(len(triangle[i])):
+                res[j] = min(res[j],res[j+1])+triangle[i][j]
+        return res[0]
