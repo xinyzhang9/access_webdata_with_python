0dd2aa52b2452a46836bad9adaca8bd1708b78cc
Xinyang <xinyzhang9@gmail.com>
2016-07-29 12:19:59 -0700

    dp TLE

--- leetcode_python/perfectSquares.py
+++ leetcode_python/perfectSquares.py
@@ -27,4 +27,23 @@ class Solution(object):
             toCheck = nextCheck
         
         return cnt
-        
+
+
+
+# dp solution, TLE
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <=0:
+            return 0
+        dp = [sys.maxint]*(n+1)
+        dp[0] = 0
+        for i in range(1,len(dp)):
+            j = 1
+            while j*j <= i:
+                dp[i] = min(dp[i],dp[i-j*j]+1)
+                j+=1
+        return dp[n]
