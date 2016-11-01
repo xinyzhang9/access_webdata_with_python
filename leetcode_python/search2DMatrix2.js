393b5c3a938b92dc9a2e0c6d36dff3975b7ff773
Xinyang <xinyzhang9@gmail.com>
2016-11-01 12:22:38 -0700

    search2dmatrix2.js

---
+++ leetcode_python/search2DMatrix2.js
@@ -0,0 +1,36 @@
+// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+// Integers in each row are sorted in ascending from left to right.
+// Integers in each column are sorted in ascending from top to bottom.
+// For example,
+
+// Consider the following matrix:
+
+// [
+//   [1,   4,  7, 11, 15],
+//   [2,   5,  8, 12, 19],
+//   [3,   6,  9, 16, 22],
+//   [10, 13, 14, 17, 24],
+//   [18, 21, 23, 26, 30]
+// ]
+// Given target = 5, return true.
+
+// Given target = 20, return false.
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let r = 0;
+    let c = n-1;
+    while(r <= m-1 && c >=0 ){
+        if(matrix[r][c] === target) return true;
+        else if(matrix[r][c] > target) c--;
+        else r++;
+    }
+    return false;
+};
